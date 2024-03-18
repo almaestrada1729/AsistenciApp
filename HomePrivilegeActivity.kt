6a2673670705856321dc712cbd73671bb71a6890
almaestrada1729 <142361867+almaestrada1729@users.noreply.github.com>
2024-03-17 18:58:25 -0700

    Update HomePrivilegeActivity.kt

--- HomePrivilegeActivity.kt
+++ HomePrivilegeActivity.kt
@@ -7,14 +7,24 @@ import android.icu.util.Calendar
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.MenuItem
+import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.PopupMenu
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import com.almadevs.androidcurso.LoginActivity
 import com.almadevs.androidcurso.R
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import java.util.Locale
 
 class HomePrivilegeActivity : AppCompatActivity() {
@@ -25,11 +35,13 @@ class HomePrivilegeActivity : AppCompatActivity() {
     private lateinit var btnReport: MenuItem
     private lateinit var btnListEmp: MenuItem
 
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint("MissingInflatedId", "WrongViewCast")
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_privilege)
+        val nombre_usuario = intent.getStringExtra("nombre_usuario")
+        findViewById<TextView>(R.id.nombreEmpleadoPrivilege).text = nombre_usuario
         // Obtener la referencia al TextView
         val textDate = findViewById<TextView>(R.id.textDatePrivilege)
         // Obtener la fecha actual
@@ -37,14 +49,27 @@ class HomePrivilegeActivity : AppCompatActivity() {
         val dateFormat = SimpleDateFormat("dd 'de' MMMM 'de' yyyy", Locale.getDefault())
         val fechaActual = dateFormat.format(calendar.time)
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.collectionMenuBottomNavigation)
+        //Obtener referencia del menu mas
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbarHomePage)
+
+        //Terminos y condiciones desde el menu
+        val dialogView = LayoutInflater.from(this).inflate(R.layout.custom_dialog_layout, null)
+        val buttonAceptar = dialogView.findViewById<Button>(R.id.buttonAceptar)
 
         // Obtener una referencia al menú inflado
         val menuInflater = menuInflater
         val menu = bottomNavigationView.menu
-
         // Obtener las referencias a los elementos del menú
         btnReport = menu.findItem(R.id.btnReport)
         btnListEmp = menu.findItem(R.id.btnListEmp)
+
+        val builder = AlertDialog.Builder(this)
+        builder.setView(dialogView)
+        val dialog = builder.create()
+
+        buttonAceptar.setOnClickListener {
+            dialog.dismiss() // Cierra el diálogo
+        }
 
         // Establecer la fecha actual en el TextView
         textDate.text = fechaActual
@@ -61,6 +86,32 @@ class HomePrivilegeActivity : AppCompatActivity() {
             navigateToListEmpl()
             true
         }
+
+        toolbar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.informacion -> {
+                    // Manejar la acción del elemento "Información"
+                    dialog.show()
+                    true
+                }
+                R.id.cerrar_sesion -> {
+                    // Manejar la acción del elemento "Cerrar Sesión"
+                   AlertDialog.Builder(this)
+                        .setTitle("Cerrar Sesión")
+                        .setMessage("¿Estás seguro de que deseas cerrar la sesión?")
+                        .setPositiveButton("Aceptar") { dialog, which ->
+                            val intent = Intent(this, LoginActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                        .setNegativeButton("Cancelar", null)
+                        .show()
+                    true
+                }
+                else -> false
+            }
+        }
+
     }
 
     private fun navigateToReport() {
